d3a2c119b417b5f9953219ed1ba6291e50e17e06
widuu <admin@widuu.com>
2013-08-06 16:44:59 +0800

    go test strcut

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+type User struct{
+	Member string
+	Age int
+}
+func main(){
+	a:=User{Member:"widuu",Age:12}
+	a.test()
+	fmt.Println(a)
+}
+func (u *User) test{
+	fmt.Println(u.Age)
+}
